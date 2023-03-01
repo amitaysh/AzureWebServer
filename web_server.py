30a79817667426c368eeac276b35fc54e64b9d80
Amitay Shahar <amitayshahar@microsoft.com>
2023-03-01 21:10:54 +0200

    python change

--- web_server.py
+++ web_server.py
@@ -1,29 +1,9 @@
-# Python 3 server example
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import time
+from flask import Flask
+app = Flask(__name__)
 
-hostName = "localhost"
-serverPort = 5005
+@app.route("/")
+def hello():
+    return "Amitay Shahar was here!"
 
-class MyServer(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(bytes("<html><head><title>Amitay_Shahar</title></head>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>This is an example web server, deployed automatically to azure cluster.</p>", "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
-
-if __name__ == "__main__":        
-    webServer = HTTPServer((hostName, serverPort), MyServer)
-    print("Server started http://%s:%s" % (hostName, serverPort))
-
-    try:
-        webServer.serve_forever()
-    except KeyboardInterrupt:
-        pass
-
-    webServer.server_close()
-    print("Server stopped.")
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
