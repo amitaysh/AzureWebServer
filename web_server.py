fcab4c7b5e16019bc54ef6f4bb733848cfa4d4cd
Amitay Shahar <amitayshahar@microsoft.com>
2023-03-02 12:05:06 +0200

    update message

--- web_server.py
+++ web_server.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Amitay Shahar was here!"
+    return "Amitay Shahar and Guy was here!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
